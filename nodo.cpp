761e28ea596d11ff660a53c4b22c22113b2d1b70
gabrielaelisa <gabriela.mendoza@ug.uchile.cl>
2019-04-24 14:22:52 -0400

    cambios

--- nodo.cpp
+++ nodo.cpp
@@ -5,10 +5,6 @@ using namespace std;
 class Nodo {
 
 public:
-    //ATRIBUTOS
-
-    // constructor clase nodo, recibe cuatro vertices posicionados en el espacio 
-    Nodo(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4);
 
     // constructor que deserializa una linea de un archivo 
     Nodo(string line);
@@ -19,7 +15,20 @@ public:
 private:
     //llave del nodo
     int x1, y1, x2, y2, x3, y3, x4, y4;
-    # puntero a hijos
+     //puntero a hijos
     list<Nodo> childs;
     
 };
+class Rectangulo {
+public:
+    Nodo nodo_actual;
+    Nodo hijo; // añadir la opción de tener como hijo una hoja?
+    list<int> MBR;
+
+    //constructor
+    Rectangulo(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4){
+        
+    }
+        //agregar los cuatro vertices a la lista de puntos
+    
+}
