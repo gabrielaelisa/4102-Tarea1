24543f14ac9c22781b45c3e0b33347f8127a71e3
gabrielaelisa <gabriela.mendoza@ug.uchile.cl>
2019-04-25 00:07:03 -0400

    cosas

--- nodo.cpp
+++ nodo.cpp
@@ -1,6 +1,8 @@
 #include <list>
 #include <string>
 #include <bitset>
+#include <fstream>
+#include <stdio.h>
 using namespace std;
 std::list<int>::iterator it;
  
@@ -10,15 +12,30 @@ class Rectangulo;
 class Nodo;
 
 class Nodo{
+
+    fstream archivo;
+
 public: 
 //aqui deben ir los constructores
-    Nodo();
-    // aca se debe serializar un nodo a un binario y escribirlo en un archivo
-    string toString(){
-        for(it= rect_hijos.begin())
+    
+    // constructor raiz
+    Nodo(char * id, Rectangulo * rec){
+        id_archivo= id;
+        archivo.open(id_archivo, ios::binary | ios::ate);
+        archivo << rec->MBR;
+        archivo.close;
+
 
     }
+    // constructor nodo normal
+    Nodo(char * id, Rectangulo rec, Rectangulo* padre)
+    {
+        rect_padre= padre;
+        id_archivo= id;
 
+    }
+    // aca se debe serializar un nodo a un binario y escribirlo en un archivo
+    
     void instert_rectangle(){
 
     }
@@ -28,18 +45,22 @@ private:
     Rectangulo * rect_padre;
     // lista de rectangulos hijos
     list<Rectangulo> rect_hijos;
+    char * id_archivo;
 
 };
 
 /* un rectangulo respresenta la MBR del nodo al cual apunta*/
 class Rectangulo{
+    
 public:
+    char * MBR;
 
     //constructor para deserializar una linea de un archivo .txt 
     Rectangulo(){
     // aqui debe agregar los cuatro vertices al tributo mbr
     
     }
+    /*
     string serialize(){
         string serialized; 
         for (it= MBR.begin(); it!= MBR.end(); it++){
@@ -50,12 +71,12 @@ public:
             }
             
         }
-    }
+    }*/
 private:
 
     Nodo * nodo_actual;
     Nodo * nodo_hijo; // añadir la opción de tener como hijo una hoja?
-    list<int> MBR;
+    //list<int> MBR;
+    
 
 };
-
