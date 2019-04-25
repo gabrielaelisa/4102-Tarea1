c719a94a97388c5ba9e17b0dc182712dac265ba6
gabrielaelisa <gabriela.mendoza@ug.uchile.cl>
2019-04-25 18:21:57 -0400

    cambios a las clases nodo y rectangulo

--- nodo.cpp
+++ nodo.cpp
@@ -1,11 +1,21 @@
 #include <list>
 #include <string>
+#include <string.h> 
+#include <stdlib.h>
 #include <bitset>
 #include <fstream>
 #include <stdio.h>
 using namespace std;
 std::list<int>::iterator it;
+FILE * arbol;
  
+// Cantidad de datos
+#define M 10000
+#define TAMANO_LINEA 8+M*36
+#define NODO_INTERNO 0
+#define NODO_HOJA 1
+#define MBR 0
+#define DATO 1
 
 /* clase nodo, un nodo almacena hasta M rectangulos*/
 class Rectangulo;
@@ -14,37 +24,60 @@ class Nodo;
 class Nodo{
 
 public: 
-//aqui deben ir los constructores
+    int tipo=NODO_INTERNO; // (0 interno, 1 hoja)
+    // en que parte del archivo comienza
+    int cantidadElementos=0;
+    int this_linea;
+    int * datos;
+    Rectangulo mbr_padre;
+  
+    Nodo(int linea, Rectangulo  rec, Rectangulo * padre){
+        datos= (int*) malloc(TAMANO_LINEA);
+        if(rec.tipo==DATO) tipo=NODO_HOJA;
+        this_linea=linea;
+
+        //raiz
+        if (padre==NULL){
+            arbol = fopen("archivo_tarea", "rw");
+            setbuf(arbol, NULL);
+            append(rec);
+        }
+
+        // nodo
+        else
+        {
+            mbr_padre= *padre;
+            append(rec);
+        }  
+       
+    }
+    void append(Rectangulo rec){
+        set_rect(cantidadElementos, rec);
+        cantidadElementos++;
+    }
     
-    // constructor raiz
-    Nodo(char * id, Rectangulo * rec){
-        //crea el identificador del archivo que va a almacenar en disco
-        es_raiz=true;
-        id_archivo= id;
-        FILE * f= fopen(id_archivo,"rw");
-        fwrite(rec->MBR , 1 , sizeof(rec->MBR) , f );
-        fclose(f);
-
-    }
-    // constructor nodo normal
-    Nodo(char * id, Rectangulo rec, Rectangulo* padre)
-    {
-        es_raiz=false;
-        rect_padre= padre;
-        id_archivo= id;
-
-    }
-    // aca se debe serializar un nodo a un binario y escribirlo en un archivo
-    
-    void instert_rectangle(){
-
+    void set_rect(int pos, Rectangulo rec){
+        if (tipo==NODO_HOJA){
+            memcpy(datos+8 + pos*36, rec.puntos, 32);
+        }
+        else{            
+            memcpy(datos+8 + pos*36, &rec.nodo_hijo, 4);            
+            memcpy(datos+8 + pos*36 +4, rec.puntos, 32);
+        }     
     }
 
-private:
-    // rectangulo padre
-    bool es_raiz;
-    Rectangulo * rect_padre;
-    char * id_archivo;
+
+    Rectangulo get_rectangulo(int pos){ 
+        int p= 4+36*pos+4;
+        int tipoRect= tipo==NODO_HOJA? DATO: MBR;
+        return Rectangulo(datos+p, tipoRect);
+    }
+
+    // aqui se implementa el metodo para mandar el nodo a disco cuando se descienda por algun rectangulo
+    void guardar(){
+        fseek(arbol, this_linea, SEEK_SET);
+        fwrite(datos, 1 , TAMANO_LINEA, arbol );
+    }
 
 };
 
@@ -52,18 +85,18 @@ private:
 class Rectangulo{
     
 public:
-    char * MBR;
-
-
-    Rectangulo(){
-    // aqui debe agregar los cuatro vertices al tributo mbr
-    
-    }
-    
-private:
-
+    int tipo;
+    int puntos[8];
     Nodo * nodo_actual;
-    Nodo * nodo_hijo; // añadir la opción de tener como hijo una hoja?
-    
+    int nodo_hijo;
+
+    Rectangulo(int* pts, int tipo){
+        tipo=tipo;
+        for(int i= 0; i<8; i++){
+            puntos[i]=pts[i];
+        }
+    }  
+
+    int area
 
 };
